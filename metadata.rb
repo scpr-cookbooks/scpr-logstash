1a9c2c7248b4f4e8b3327a9fc192a4fffd4699a3
Eric Richardson <e@ericrichardson.com>
2014-12-24 11:16:13 -0500

    scpr-logstash 0.1.17: Add geoip lookup to nginx records

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'erichardson@scpr.org'
 license          'all_rights'
 description      'Installs/Configures scpr-logstash'
 long_description 'Installs/Configures scpr-logstash'
-version          '0.1.16'
+version          '0.1.17'
 
 depends "apt"
 depends "logstash"
